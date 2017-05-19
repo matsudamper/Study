71382c489002fb475fa352365373e3eeb02b4ad6
matsudamper <matsudamper971227@gmail.com>
2017-05-20 00:36:20 +0900

    Mapでvalueでソートを実装

---
+++ Kotlin/asort.kt
@@ -0,0 +1,10 @@
+/*
+thenByの中身について考察の余地あり
+hashCode衝突しないかとか
+オブジェクト同士を比較できる方法があれば良い
+ */
+fun <K, V : Comparable<V>> MutableMap<K, V>.asort(): MutableMap<K, V>
+        = this.toSortedMap(compareBy<K> { this[it] }.thenBy { it!!.hashCode() })
+
+fun <K, V : Comparable<V>> MutableMap<K, V>.arsort(): MutableMap<K, V>
+        = this.toSortedMap(compareByDescending<K> { this[it] }.thenBy { it!!.hashCode() })
